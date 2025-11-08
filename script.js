85881dc3709ae5b06b94e6758ffd5f8c56ea5289
omarjaafari <omarjaafari2006@gmail.com>
2025-11-08 09:33:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let clickCount = 0;
+function greetUser() {
+  const nameInput = document.getElementById("nameInput");
+  const outputDiv = document.getElementById("output");
+  const clickCounter = document.getElementById("clickCounter");
+  const body = document.body;
+  const name = nameInput.value.trim();
+
+  clickCount++;
+
+  if (name === "") {
+    outputDiv.innerText = "Please enter your name before clicking!";
+    outputDiv.style.color = "red";
+  } else {
+    outputDiv.innerText = `Hello, ${name}!`;
+    outputDiv.style.backgroundColor = "#222"
+    outputDiv.style.borderRadius = "10px"
+    outputDiv.style.padding = "20px"
+  }
+
+  clickCounter.innerText = `Button clicked: ${clickCount} times`;
+}
+
+document.getElementById("greetBtn").addEventListener("click", greetUser);
